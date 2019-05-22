33103998fb18a66f0fc6b8401a95150dcbec30e3
nahum <nahum.trinidadv@gmail.com>
2019-05-21 23:49:56 -0500

    Add custom user and superuser with TDD

--- app/core/models.py
+++ app/core/models.py
@@ -8,11 +8,23 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create and saves a new user"""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+
+    def create_superuser(self, email, password=None):
+        """create superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that suppors using email instead of username"""
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,11 +7,34 @@ class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """ Test """
         email = 'nahum.trinidadv@gmail.com'
-        password = '123456'
+        password = 'abc123456'
         user = get_user_model().objects.create_user(
             email=email,
-            passsword=password
+            password=password
         )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_email_normalize(self):
+        """New email normalize"""
+        email = 'test@CREHANAnahum.com'
+        user = get_user_model().objects.create_user(email, 'nahumtrinidad')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """ test invalid email"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_new_superuser(self):
+        """ Test creation new user of type superuser"""
+
+        user = get_user_model().objects.create_superuser(
+            'admin@nahum.com',
+            'admin123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
